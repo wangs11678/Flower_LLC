13650c5cc3a21bc6bf0f6ffcd87f85d4d7fc30a1
wangs11678 <826473029@qq.com>
2017-01-08 19:17:13 +0800

    update20170108

--- Flower_LLC/predict.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include "liblinear\linear.h"
-
-void SVM_predict(int argc, char **argv, char *testFile, char *modelFile, char *resultFile);
